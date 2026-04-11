f1efa90c4d386a1a0bb11006533a54d77426e2b0
EG2603 <104365379+EG2603@users.noreply.github.com>
2026-04-11 20:50:04 +0300

    Add files via upload

---
+++ apps.nix
@@ -0,0 +1,41 @@
+
+{ config, lib, pkgs, ... }:
+
+{
+  # programs.firefox.enable = true;
+    programs.chromium.enable = true;
+    programs.thunar.enable = true;
+    programs.thunar.plugins = with pkgs; [
+	xfce.thunar-volman
+	xfce.thunar-archive-plugin
+	xfce.thunar-media-tags-plugin
+    ];
+
+
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+   environment.systemPackages = with pkgs; [
+   	vim
+	kdePackages.kate
+	wget
+#	thunar
+#	xfce.thunar-volman
+#	thunar-archive-plugin
+#	thunar-media-tags-plugin
+	xfce.tumbler
+	gvfs
+	ffmpeg
+	vlc
+	fastfetch
+	openbox
+	obconf
+	xterm
+	xorg.xinit
+	neovim
+	chromium
+   ];
+
+
+
+
+}

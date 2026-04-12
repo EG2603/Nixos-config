04f6d06a93c278f5f6617cc07d6a634a3e58e515
EG2603 <egegurdal0@gmail.com>
2026-04-12 22:25:49 +0300

    .

--- copy.sh
+++ copy.sh
@@ -5,19 +5,18 @@ git clone https://github.com/grassmunk/Chicago95.git
 git clone https://github.com/B00merang-Project/Windows-Vista.git
 git clone https://github.com/dangvd/crystal-remix-icon-theme.git
 #------------------------------------------------------
-sudo mkdir -p /usr/share/themes
-sudo mkdir -p /usr/share/icons
-sudo cp -r crystal-remix-icon-theme /usr/share/icons
-sudo cp -r Windows-Vista /usr/share/themes
-sudo cp -r Chicago95/Theme/Chicago95 /usr/share/themes
-sudo cp -r Chicago95/Icons/Chicago95 /usr/share/icons
-sudo cp -r Chicago95/Cursors/Chicago95_Standard_Cursors /usr/share/icons
+sudo mkdir -p ~/.local/share/themes
+sudo mkdir -p ~/.local/share/icons
+sudo cp -r crystal-remix-icon-theme ~/.local/share/icons
+sudo cp -r Windows-Vista ~/.local/share/themes
+sudo cp -r Chicago95/Theme/Chicago95 ~/.local/share/themes
+sudo cp -r Chicago95/Icons/Chicago95 ~/.local/share/icons
+sudo cp -r Chicago95/Cursors/Chicago95_Standard_Cursors ~/.local/share/icons
 #------------------------------------------------------
 sudo cp apps.nix /etc/nixos/apps.nix
 sudo cp configuration.nix /etc/nixos/configuration.nix
 sudo cp home-manager.nix /etc/nixos/home-manager.nix
 cp -r home/. ~
 cp -r home/.config  ~
-gtk-update-icon-theme
 sudo nixos-rebuild switch
 echo installation finished

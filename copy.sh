0cb4b0991944f1f8d8f0e6faa039c9fafacebcaf
EG2603 <egegurdal0@gmail.com>
2026-04-12 21:32:17 +0300

    .

--- copy.sh
+++ copy.sh
@@ -1,5 +1,5 @@
-cp apps.nix /etc/nixos/apps.nix
-cp configuration.nix /etc/nixos/configuration.nix
-cp home-manager.nix /etc/nixos/home-manager.nix
+sudo cp apps.nix /etc/nixos/apps.nix
+sudo cp configuration.nix /etc/nixos/configuration.nix
+sudo cp home-manager.nix /etc/nixos/home-manager.nix
 cp -r home/. ~
 cp -r home/.config  ~

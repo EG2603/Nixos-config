9a93a9b62dd5f701c4af1856d53cd4f996fe0faa
EG2603 <egegurdal0@gmail.com>
2026-04-16 23:59:55 +0300

    .

--- configuration.nix
+++ configuration.nix
@@ -25,16 +25,16 @@
   # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
 
-  systemd.services.start-gui.path = ["/run/current-system/sw"];
-  systemd.services.start-gui = {
-  description = "Start GUI";
-  wantedBy = [ "multi-user.target" ];
-  serviceConfig = {
-    User = "default";
-    Type = "oneshot";
-  };
-  script = ''bash'';
-};
+#  systemd.services.start-gui.path = ["/run/current-system/sw"];
+#  systemd.services.start-gui = {
+#  description = "Start GUI";
+#  wantedBy = [ "multi-user.target" ];
+#  serviceConfig = {
+#    User = "default";
+#    Type = "oneshot";
+#  };
+#  script = ''startx'';
+#};
 
 
 
@@ -144,6 +144,7 @@ exec openbox
   # Define a user account. Don't forget to set a password with ‘passwd’.
    users.users.default = {
      isNormalUser = true;
+     shell = pkgs.bash;
      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
      packages = with pkgs; [
        tree
